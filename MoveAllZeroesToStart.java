8c736cfbe7f385476e3edff74d01aa8a2b97c904
Hatim Husain <hatim.husain24@gmail.com>
2026-01-01 23:59:41 +0530

    Question 2 : Code solution for move all zeroes to the start

---
+++ MoveAllZeroesToStart.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+//Problem Statement : Move all zeroes to the start
+public class MoveAllZeroesToStart {
+    public static void main(String[] args) {
+        int[] num = {1,0,4,3,0,2,0,5};
+        int nonZeroIndex = num.length-1;
+
+        for (int i=num.length-1; i>=0; i--){
+            if (num[i] !=0){
+                int temp = num[i];
+                num[i] = num[nonZeroIndex];
+                num[nonZeroIndex] = temp;
+                nonZeroIndex--;
+            }
+        }
+        System.out.println(Arrays.toString(num));
+    }
+}
