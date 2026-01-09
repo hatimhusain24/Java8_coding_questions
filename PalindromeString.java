64e766d8e7dc46e401908752529e91503cbbf11f
Hatim Husain <hatim.husain24@gmail.com>
2026-01-09 23:25:16 +0530

    Question 9 : Code solution to find given string is palindrome or not

---
+++ PalindromeString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static boolean Palindrome(String str){
+        int left = 0;
+        int right = str.length()-1;
+
+        while (left < right){
+            if (str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        boolean isPalindrome = Palindrome(str);
+        if (isPalindrome){
+            System.out.println("Given string is palindrome");
+        } else {
+            System.out.println("Given string is not palindrome");
+        }
+    }
+}
