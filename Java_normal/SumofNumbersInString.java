3e2ed9a76958d9d96c72b426c958e4a3c8e2bd1c
Hatim Husain <hatim.husain24@gmail.com>
2026-04-01 23:07:22 +0530

    Question 23 core java: Solution for summing up the numbers present in the string

---
+++ Java_normal/SumofNumbersInString.java
@@ -0,0 +1,12 @@
+package Java_normal;
+
+public class SumofNumbersInString {
+    public static void main(String[] args) {
+        String str = "a1b2c3d4e5";
+        int sum = 0;
+        for (int i=1; i<str.length(); i=i+2){
+            sum += str.charAt(i) - '0';
+        }
+        System.out.println(sum);
+    }
+}
