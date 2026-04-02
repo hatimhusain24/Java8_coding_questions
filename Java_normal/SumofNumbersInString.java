1a4159c3f95a058119f685f7633016ca231cff4e
Hatim Husain <hatim.husain24@gmail.com>
2026-04-02 23:56:41 +0530

    change

--- Java_normal/SumofNumbersInString.java
+++ Java_normal/SumofNumbersInString.java
@@ -4,7 +4,7 @@ public class SumofNumbersInString {
     public static void main(String[] args) {
         String str = "a1b2c3d4e5";
         int sum = 0;
-        for (int i=1; i<str.length(); i=i+2){
+        for (int i=1; i<str.length(); i= i+2){
             sum += str.charAt(i) - '0';
         }
         System.out.println(sum);
